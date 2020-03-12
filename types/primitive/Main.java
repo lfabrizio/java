9ac623d49d00a2ad78c42edee5ce02cab1256727
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-12 17:38:00 -0400

    primitive

---
+++ types/primitive/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    byte age = 30;
+	    long viewsCount = 3_123_456_789L;
+	    float price = 10.99f;
+	    char letter = 'A';
+	    boolean isEligible = false;
+	    
+    }
+}
